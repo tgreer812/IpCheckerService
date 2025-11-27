30f407467dd4d5d8eb4a30f64a9a49e12ab4eedc
Tyler Greer <tgreer812@gmail.com>
2025-11-27 18:31:02 -0500

    Added console entrypoint

---
+++ src/IpCheckerService.Console/Program.cs
@@ -0,0 +1,16 @@
+﻿using IpCheckerService.Worker;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+var builder = Host.CreateApplicationBuilder(args);
+
+builder.Logging.AddConsole();
+builder.Services.AddHostedService<Worker>();
+
+var host = builder.Build();
+
+Console.WriteLine("IP Checker Service - Console Mode");
+Console.WriteLine("Press Ctrl+C to stop...\n");
+
+await host.RunAsync();
